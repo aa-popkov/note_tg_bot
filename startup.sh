1222060fa25e7b38c2ed3810f753c49e52c6bac4
Alexey Popkov <AlexeyPopkov@pm.me>
2024-03-22 12:56:17 +0300

    КОСТЫЛЬ лечения алембика

--- startup.sh
+++ startup.sh
@@ -5,5 +5,9 @@ echo "Start up!"
 echo "Start Docker Containers"
 docker-compose up -d
 
+sed -i.bak 's/from models/#from models/g' ./utils/scheduler/hbd.py
+./.venv/bin/python3 -m alembic upgrade head
+mv ./utils/scheduler/hbd.py.bak ./utils/scheduler/hbd.py
+
 echo "Start Bot"
 ./.venv/bin/python3 ./run.py
