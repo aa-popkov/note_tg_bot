00b9a3d14c6921f06a91f2378ef4c0e0b370501b
Alexey Popkov <AlexeyPopkov@pm.me>
2025-05-21 14:16:11 +0300

    fix to docker

--- startup.sh
+++ startup.sh
@@ -6,7 +6,7 @@ echo "Start up!"
 #docker compose up -d
 
 sed -i.bak 's/from models/#from models/g' ./utils/scheduler/hbd.py
-./.venv/bin/python3 -m alembic upgrade head
+python3 -m alembic upgrade head
 mv ./utils/scheduler/hbd.py.bak ./utils/scheduler/hbd.py
 
 echo "Start Bot"
