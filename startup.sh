092bea79a281e5bf94f390e500092dfcc050af76
Alexey Popkov <AlexeyPopkov@pm.me>
2025-05-21 13:43:41 +0300

    fix startup.sh for docker

--- startup.sh
+++ startup.sh
@@ -2,12 +2,13 @@
 
 echo "Start up!"
 
-echo "Start Docker Containers"
-docker compose up -d
+#echo "Start Docker Containers"
+#docker compose up -d
 
 sed -i.bak 's/from models/#from models/g' ./utils/scheduler/hbd.py
 ./.venv/bin/python3 -m alembic upgrade head
 mv ./utils/scheduler/hbd.py.bak ./utils/scheduler/hbd.py
 
 echo "Start Bot"
-./.venv/bin/python3 ./run.py
+#./.venv/bin/python3 ./run.py
+python3 ./run.py
